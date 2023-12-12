21415474fb3dfac0100e3c837bece83531543d57
Marco Sallas <marco.a.s.tamassia@gmail.com>
2023-12-12 10:05:36 -0300

    Iniciando a implementação de das classes Position Board e Piece na camada BoardGame, Criação da Camada de Xadrez, com as classes ChessMatch,ChessPiece e Color, Criação da UI para impressão do tabuleiro para o User

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 	
 		
-		System.out.println("Hello Word");
+		ChessMatch chessmatch = new ChessMatch();
+		UI.printBoard(chessmatch.getPieces());
+		
 
 	}
 
